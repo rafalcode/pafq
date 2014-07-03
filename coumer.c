e01d050c59e4c3e7c8f71eed66647548d2eb977d
Ramonf <ramonfallon@gmail.com>
2014-07-03 12:53:03 +0200

    modified coumer to print out odd kmers, and drop to exponential format after 1 million

--- coumer.c
+++ coumer.c
@@ -1,9 +1,9 @@
-/* a utility to count the totla number of mers for a range of k values */
+/* a utility to count the total number of mers for a range of k values */
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-#define LIMIT 41
+#define LIMIT 46 /* 46 will take it up to a billion billion billion (1x10^27) */
 
 int main(int argc, char *argv[])
 {
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
     double dres;
     printf("Simple demonstration of kmer space depending on size of k\n");
     printf("K\tQuan\n");
-    for(i=1;i<LIMIT;++i) {
-        if(i<12) {
+    for(i=1;i<LIMIT;i+=2) {
+        if(i<10) {
             res=pow(4, i);
             printf("%i\t%u\n", i, res);
         } else {
             dres=(double)pow(4, i);
-            printf("%i\t%e\n", i, dres);
+            printf("%i\t%.3e\n", i, dres);
         }
     }
 
